2f0325300e6da05c53bcaf83a42568703f989296
rob <un@rob.mx>
2020-02-28 00:33:59 -0500

    use single headless instance to run all pdf generation

--- src/cv/pdf-render/to-pdf.js
+++ src/cv/pdf-render/to-pdf.js
@@ -1,26 +1,30 @@
 const puppeteer = require('puppeteer-core');
 const dns = require('dns').promises;
+const { URL } = require('url');
 
-const url = process.argv[2];
-const target = `/target/${process.argv[3]}`;
-
-// require('fs').mkdirSync(target.replace(/[^/]+\.pdf$/, ''))
-
-console.log(`printing ${url} to ${target}`)
+const urls = process.argv.slice(2);
 
 ;(async () => {
   try {
     const { address: hostname } = await dns.lookup('host.docker.internal')
-
+    
     const browser = await puppeteer.connect({
       browserURL: `http://${hostname}:5555`
     });
-    const page = await browser.newPage();
-    await page.goto(url, {waitUntil: 'networkidle2'});
-    await page.pdf({path: target, format: 'A4'});
-   
-    await browser.close();
+    
+    for (url_index in urls) {
+      const url = urls[url_index];
+      const { pathname } = new URL(url)
+      const target = `/target${pathname}.pdf`
+      console.log(`printing ${url} to ${target}`)
+
+      const page = await browser.newPage();
+      await page.goto(url, {waitUntil: 'networkidle2'});
+      await page.pdf({path: target, format: 'A4'});
+      await page.close();
+    }
   } catch (err) {
     console.error(err);
+    browser.close();
   }
 })();
